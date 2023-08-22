e5ba8817e0bf1d6c4b9a3a43489191a057963f2a
Martin-Waruhiu <migwiwaruhiu@gmail.com>
2023-08-22 00:39:58 -0700

    shell

--- simpleshell.h
+++ simpleshell.h
@@ -65,8 +65,6 @@ typedef struct info
 
 } information_x;
 
-//getline
-
 ssize_t read_process(information_x *ptrstruct);
 void signal_int(__attribute__((unused))int signalnumber);
 ssize_t chain_buffers(information_x *ptrstruct, char **bufer, size_t *n);
@@ -78,55 +76,49 @@ int comparison(char *sl1, char *sl2);
 char *begin(const char *hay_stack, const char *nidle);
 char *concatenation(char *destination, char *source);
 
-//getenviron
-
 char **discover_env(information_x *ptrstruct);
 int setenv_(information_x *ptrstruct, char *add_variable, char *equal);
 int unsetenv_(information_x *ptrstruct, char *rem_var);
-//exits
+
 char *find_char(char *string, char ch);
 char *strncat_(char *dest, char *src, int x);
 char *stringcopy_(char *dest, char *src, int x);
 
-//get info
 
 void release_info(information_x *ptrstruct, int whole);
 void begin_struct(information_x *ptrstruct, char **avector);
 void start_information(information_x *ptrstruct);
-//builtin
 int change_directory(information_x *ptrstruct);
 int gate_way(information_x *ptrstruct);
 int now_cd(information_x *ptrstruct);
 
-//builtin1
 int alias_output(linked_x *our_node);
 int way_back(information_x *ptrstruct);
 int fake_alias(information_x *ptrstruct);
 int remove_alias(information_x *ptrstruct, char *str);
 int give_alias(information_x *ptrstruct, char *string);
 
-//atoi
 int joining(information_x *ptrstruct);
 int alphabet(int ch);
 int separator(char ch, char *delimeter);
 int ascii_integer(char *string);
-//errors.c
+
 int putchar_error(char ch);
 void string_input(char *string);
 int write_str_fd(char *string, int FD);
 int write_fd(char ch, int FD);
-//errors1.c
+
 void output_error(information_x *ptrstruct, char *stringerror);
 int str_to_int(char *str);
 int output_d(int value, int FD);
 char *change_num(long int digit, int foundation, int flagz);
 void eliminate_comments(char *bufA);
-//HISTORY
+
 int linked_hist(information_x *ptrstruct, char *bufferA, int counter);
 int output_hist(information_x *ptrstruct);
 char *wayback_file(information_x *ptrstruct);
 int study_hist(information_x *ptrstruct);
-//ENVIRON.C
+
 int new_environment(information_x *ptrstruct);
 char *value_env(information_x *ptrstruct, char *identity);
 int now_environment(information_x *ptrstruct);
@@ -145,7 +137,7 @@ size_t outputlinkedstr(const linked_x *k);
 int eliminatenode(linked_x **hd, unsigned int index1);
 void escapelist(linked_x **hdptr);
 
-/*memory.c*/
+
 int escapepointer(void **ptrA);
 /*parser.c*/
 int execcmd(information_x *ptrstruct, char *way);
